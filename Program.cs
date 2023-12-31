f8adde74f9e9ba942eadda1c0bab71c781289446
Lucas Silva <62816701+luksalb@users.noreply.github.com>
2023-12-30 22:56:06 -0300

    feat: Adicionando Match History
    Puxando da API os IDs das partidas para depois trazer os dados delas do invocador.

--- Program.cs
+++ Program.cs
@@ -7,12 +7,13 @@ class Program
 {
     static async Task Main()
     {
-        // Substitua "SUA_CHAVE_DE_API" pela chave de API fornecida pela Riot Games
+        // Substitua "SUA_CHAVE_DE_API" pela chave de API fornecida pela Riot Games e summonerName "Nome%20Invocador" pelo que você quer ver
         string apiKey = "SUA_CHAVE_DE_API";
-        string summonerName = "Nome%20Invocador";
+        string summonerName = "Manopla%20Quebrada";
 
         // Substitua "REGIAO" pela região do servidor (por exemplo, "br1" para o servidor brasileiro)
-        string region = "REGIAO";
+        string region = "br1";
+        string regionHistory = "AMERICAS";
 
         // Obtendo o ID do invocador usando o endpoint Summoner API
         SummonerData summonerData = await GetSummonerData(apiKey, summonerName, region);
@@ -21,38 +22,37 @@ class Program
         {
             // Exibir informações formatadas
             Console.WriteLine($"Summoner Id: {summonerData.Id}");
+            Console.WriteLine($"Summoner PuuId: {summonerData.Puuid}");
             Console.WriteLine($"Summoner Name: {summonerData.Name}");
             Console.WriteLine($"Profile Icon Id: {summonerData.ProfileIconId}");
             Console.WriteLine($"Summoner Level: {summonerData.SummonerLevel}\n");
-        }
-        else
-        {
-            Console.WriteLine("Invocador não encontrado.");
-        }
 
-        List<RankedData> rankedDataList = await GetRankedData(apiKey, summonerData, region);
+            List<string> SummonerMatchIds = await GetMatchId(apiKey, summonerData, regionHistory);
+            Console.WriteLine($"Summoner match Id: {SummonerMatchIds.Count}\n");
 
-        if (rankedDataList != null && rankedDataList.Count > 0)
-        {
-            // Iterar sobre a lista de RankedData
-            foreach (var rankedData in rankedDataList)
+            List<RankedData> rankedDataList = await GetRankedData(apiKey, summonerData, region);
+
+            if (rankedDataList != null && rankedDataList.Count > 0)
             {
-                Console.WriteLine($"Summoner ranked Id: {rankedData.LeagueId}");
-                Console.WriteLine($"Summoner ranked Type: {rankedData.QueueType}");
-                Console.WriteLine($"Summoner ranked Tier: {rankedData.Tier}");
-                Console.WriteLine($"Summoner ranked rank: {rankedData.Rank}");
-                Console.WriteLine($"Summoner ranked LP: {rankedData.LeaguePoints}");
-                Console.WriteLine($"Summoner rank Wins: {rankedData.Wins}");
-                Console.WriteLine($"Summoner rank Losses: {rankedData.Losses}\n");
+                // Iterar sobre a lista de RankedData
+                foreach (var rankedData in rankedDataList)
+                {
+                    Console.WriteLine($"Summoner ranked Id: {rankedData.LeagueId}");
+                    Console.WriteLine($"Summoner ranked Type: {rankedData.QueueType}");
+                    Console.WriteLine($"Summoner ranked Tier: {rankedData.Tier}");
+                    Console.WriteLine($"Summoner ranked rank: {rankedData.Rank}");
+                    Console.WriteLine($"Summoner ranked LP: {rankedData.LeaguePoints}");
+                    Console.WriteLine($"Summoner rank Wins: {rankedData.Wins}");
+                    Console.WriteLine($"Summoner rank Losses: {rankedData.Losses}\n");
+                }
             }
-        }
-        else
-        {
-            Console.WriteLine("Nenhuma informação de ranque encontrada para este invocador.");
-        }
+            else
+            {
+                Console.WriteLine("Nenhuma informação de ranque encontrada para este invocador.");
+            }
 
+        }
     }
-
     static async Task<SummonerData> GetSummonerData(string apiKey, string summonerName, string region)
     {
         using (HttpClient client = new HttpClient())
@@ -101,14 +101,39 @@ class Program
             }
         }
     }
+    static async Task<List<String>> GetMatchId(string apiKey, SummonerData summonerData, string regionHistory)
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            string matchListUrl = $"https://{regionHistory}.api.riotgames.com/lol/match/v5/matches/by-puuid/{summonerData.Puuid}/ids?api_key={apiKey}";
 
+            HttpResponseMessage response = await client.GetAsync(matchListUrl);
 
+            if (response.IsSuccessStatusCode)
+            {
+
+                // Usando Newtonsoft.Json para desserializar o JSON
+                string responseBody = await response.Content.ReadAsStringAsync();
+                // Use Newtonsoft.Json to deserialize the JSON
+                List<string> matchIdList = JsonConvert.DeserializeObject<List<string>>(responseBody);
+
+                return matchIdList;
+            }
+            else
+            {
+                Console.WriteLine($"Error retrieving match references: {response.StatusCode} - {response.ReasonPhrase}");
+                return null;
+            }
+        }
+    }
 }
+
 
 // Classe para representar os dados do invocador
 public class SummonerData
 {
     public string? Id { get; set; }
+    public string? Puuid { get; set; }
     public string? Name { get; set; }
     public int ProfileIconId { get; set; }
     public long SummonerLevel { get; set; }
